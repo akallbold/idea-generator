b13724bfe59c4f69ee2434b34df68c3a8b3cf873
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-12-09 11:52:53 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import useAssistantApi from "./hooks/useAssistant";
 
 function App() {
+  const { data, submitObjects, loading, error } = useAssistantApi();
+  const [inputObject1, setInputObject1] = useState("");
+  const [inputObject2, setInputObject2] = useState("");
+  const [object1, setObject1] = useState("");
+  const [object2, setObject2] = useState("");
+  const renderResults = () => {
+    if (data) {
+      // console.log("the whole thing", data);
+      console.log("data.data", data.data);
+      // console.log("data.body", data.body);
+      // console.log("data.options", data.options);
+      // console.log("data.response", data.response);
+    }
+  };
+  renderResults();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-bg h-screen bg-cover bg-center relative backdrop-blur-lg">
+      <div className="card w-3/4 h-3/4 bg-white mx-auto relative top-1/2 transform -translate-y-1/2 shadow-lg">
+        <div className="card-top h-1/2 bg-no-repeat bg-contain bg-center"></div>
+        <div className="card-bottom h-1/2 flex-row" flex-row>
+          <div className="w-1/2 flex-col">
+            <div className="flex-row">
+              <input
+                type="text"
+                placeholder="Object 1"
+                className="card-input rounded-xl"
+                value={inputObject1}
+                onChange={(e) => setInputObject1(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Object 2"
+                className="card-input rounded-xl"
+                value={inputObject2}
+                onChange={(e) => setInputObject2(e.target.value)}
+              />
+            </div>
+            <div>
+              <button
+                className="btn card-button rounded-xl"
+                onClick={() => {
+                  setObject1(inputObject1);
+                  setObject2(inputObject2);
+                  submitObjects(object1, object2);
+                  setInputObject1("");
+                  setInputObject2("");
+                }}
+              >
+                Generate Idea
+              </button>
+            </div>
+          </div>
+          <div className="w-1/2 flex-col">
+            <div className="flex-row">
+              {loading && (
+                <p>
+                  {` By combining a ${object1} and a ${object2} ChatGPT is
+                  thinking of something real good!`}
+                </p>
+              )}
+              {object1 && object2 && (
+                <p>
+                  {` Object 1 was: ${object1} \n
+                  Object 2 was ${object2} `}
+                </p>
+              )}
+              {data && <p> data... </p>}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/randomObjects.ts
@@ -0,0 +1,13 @@
+const things = [
+  "tomato",
+  "magnet",
+  "yoga mat",
+  "paperclip",
+  "coffee mug",
+  "vacuum",
+  "toothbrush",
+  "oil filter",
+  "drivers license",
+];
+
+export default things;
